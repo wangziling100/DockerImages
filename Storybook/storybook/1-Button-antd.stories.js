5e3640c0bcbfc3edafcaf49c2eb572ac6467ca50
Bob Wang <wangziling1000@gmail.com>
2020-08-03 17:14:47 +0200

    storybook, support tailwind

--- Storybook/storybook/1-Button-antd.stories.js
+++ Storybook/storybook/1-Button-antd.stories.js
@@ -4,13 +4,15 @@ import { action } from '@storybook/addon-actions';
 import {Button} from 'antd';
 import "antd/lib/button/style";
 import 'antd/dist/antd.css';
+import '../css/tailwind.css';
 
 export default {
   title: 'Button',
   component: Button,
 };
 
-export const Text = () => <Button type="primary" onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => <Button className="font-semibold" type="primary" onClick={action('clicked')}>Hello Button</Button>;
+export const Text2 = () => <Button className="text-black" type="primary"> Hello Button </Button>
 
 export const Emoji = () => (
   <Button type="primary" onClick={action('clicked')}>
--- Storybook/storybook/main.js
+++ Storybook/storybook/main.js
@@ -21,6 +21,60 @@ module.exports = {
       ],
       include: path.resolve(__dirname, '../src/'),
     });
-    return config
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          // Filter out the default .css and .module.css rules and replace them with our own.
+          ...(config.module.rules = config.module.rules.map((f) => {
+            if (f.oneOf === undefined) {
+              return f
+            }
+
+            return {
+              oneOf: f.oneOf.map((r) => {
+                if (r.test === undefined) {
+                  return r
+                }
+
+                if (r.test.toString() === '/\\.css$/') {
+                  return {
+                    test: /\.css$/,
+                    exclude: [/\.module\.css$/, /@storybook/],
+                    include: path.resolve(__dirname, "../"),
+                    use: [
+                      'style-loader',
+                      {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1, sourceMap: false },
+                      },
+                      'postcss-loader',
+                    ],
+                  }
+                }
+                if (r.test.toString() === '/\\.module\\.css$/') {
+                  return {
+                    test: /\.module\.css$/,
+                    exclude: [/@storybook/],
+                    include: path.resolve(__dirname, "../"),
+                    use: [
+                      'style-loader',
+                      {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1, sourceMap: false, modules: true },
+                      },
+                      'postcss-loader',
+                    ],
+                  }
+                }
+                return r
+              }),
+            }
+          })),
+        ],
+      },
+    }
+    //return config
   },
 };
