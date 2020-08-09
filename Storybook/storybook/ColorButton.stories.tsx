497266c328a9630633954e993e6e7d3af0afa702
Bob Wang <wangziling1000@gmail.com>
2020-08-09 12:22:13 +0200

    tailwind example

--- Storybook/storybook/ColorButton.stories.tsx
+++ Storybook/storybook/ColorButton.stories.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import ColorButton from "../components/ColorButton";
+import "../css/tailwind.css";
 storiesOf("ColorButton", module)
     .add("red",
-        () => <ColorButton color="red" />
+        () => <div className="bg-gray-500 text-red-500">test </div>
     )
     .add("blue",
         () => <ColorButton color="blue" />
